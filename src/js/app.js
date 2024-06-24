55910de1110c982123b0ae2f250a3eee89292912
Jesus Costumbre <jesuscostumbre11@gmail.com>
2024-06-24 17:30:22 -0600

    Agregamos el diseño a la galeria con sass

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,16 @@
-alert('Hola perros');
+document.addEventListener('DOMContentLoaded', function() {
+    crearGaleria();
+})
+
+function crearGaleria(){
+    const galeria = document.querySelector('.galeria-imagenes')
+    const numImagenes =16;
+
+    for(let i = 1; i <= numImagenes; i++){
+        const imagen = document.createElement('IMG')
+        imagen.src = `src/img/gallery/full/${i}.jpg`
+        imagen.alt = 'Imagen Galeria'
+        
+        galeria.appendChild(imagen)
+    }
+}
