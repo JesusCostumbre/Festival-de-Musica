8489b95e7025b806c1f6ebd133b3e6cce5bdb3c9
Jesus Costumbre <jesuscostumbre11@gmail.com>
2024-06-24 23:21:39 -0600

    terminando el proyecto

--- build/js/app.js
+++ build/js/app.js
@@ -1,7 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
+    navegacionFija();
     crearGaleria();
+    resaltarEnlace();
+    scrollNav();
 })
+function navegacionFija(){
+    const header = document.querySelector('.header')
+    const sobreFestival = document.querySelector('.sobre-festival')
 
+    window.addEventListener('scroll', function(){
+        if(sobreFestival.getBoundingClientRect().bottom < 1){
+            header.classList.add('fixed')
+        }
+        else{
+            header.classList.remove('fixed')
+        }
+    })
+}
 function crearGaleria(){
     const galeria = document.querySelector('.galeria-imagenes')
     const numImagenes =16;
@@ -63,6 +78,43 @@ function cerrarModal(){
     
 }
 
+function resaltarEnlace(){
+    document.addEventListener('scroll', function() {
+        const sections = document.querySelectorAll('section')
+        const navLinks = document.querySelectorAll('.navegacion-principal a')
 
+        let actual = '';
+        sections.forEach(section => {
+            
+            const sectionTop = section.offsetTop
+            const sectionHeight = section.clientHeight
+
+            if(window.scrollY >= (sectionTop - sectionHeight / 3)) {
+                actual = section.id
+            }
+        })
+
+        navLinks.forEach(link => {
+            link.classList.remove('active')
+            if(link.getAttribute('href') === '#' + actual){
+                 link.classList.add('active')
+            }
+        })
+    })
+}
+
+function scrollNav(){
+    const navLinks = document.querySelectorAll('.navegacion-principal  a')
+
+    navLinks.forEach( link => {
+        link.addEventListener('click', e => {
+            e.preventDefault()
+            const sectionScroll = e.target.getAttribute('href')
+            const section = document.querySelector(sectionScroll)
+
+            section.scrollIntoView({behavior: 'smooth'})
+        })
+    })
+}
 
 
