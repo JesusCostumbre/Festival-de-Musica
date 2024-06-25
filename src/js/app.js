6d2b7f3a395b9d994b162e134a575149cb50a483
Jesus Costumbre <jesuscostumbre11@gmail.com>
2024-06-24 18:43:11 -0600

    terminando la parte del modal, la transicion de la imagen de la galeria

--- src/js/app.js
+++ src/js/app.js
@@ -11,6 +11,58 @@ function crearGaleria(){
         imagen.src = `src/img/gallery/full/${i}.jpg`
         imagen.alt = 'Imagen Galeria'
         
+
+        //Event Handler 
+        // Es el proceeso de detectar y responder a una interaccion de un usuario
+        imagen.onclick = function () {
+            mostrarImagen(i)
+        }
+
         galeria.appendChild(imagen)
     }
 }
+
+function mostrarImagen(i){
+
+    const imagen = document.createElement('IMG')
+    imagen.src = `src/img/gallery/full/${i}.jpg`
+    imagen.alt = 'Imagen Galeria'
+
+    //Generar modal 
+    const modal = document.createElement('DIV')
+    modal.classList.add('modal')
+    modal.onclick = cerrarModal
+
+    
+
+    //Boton cerrar modal
+    const cerrarModalBtn = document.createElement('BUTTON')
+    cerrarModalBtn.textContent = 'X'
+    cerrarModalBtn.classList.add('btn-cerrar')
+    cerrarModalBtn.onclick = cerrarModal
+    
+    modal.appendChild(imagen)
+    modal.appendChild(cerrarModalBtn)
+
+    //Agregar al HTML
+    const body = document.querySelector('body')
+    body.classList.add('overflow-hidden')
+    body.appendChild(modal)
+}
+
+function cerrarModal(){
+    const modal = document.querySelector('.modal')
+    modal.classList.add('fade-out')
+
+    setTimeout( () => {
+        modal?.remove();
+
+    const body = document.querySelector('body')
+    body.classList.remove('overflow-hidden')
+    },500)
+    
+}
+
+
+
+
